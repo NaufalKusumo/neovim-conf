ce0347ebb8af6490a520ed57931b70c41d4854a2
NaufalKusumo <nopalji375@gmail.com>
2026-01-08 21:21:12 +0700

    f

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,32 @@
-require("nvchad.configs.lspconfig").defaults()
+local configs = require "nvchad.configs.lspconfig"
+local defaults = configs.defaults()
+local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "pylsp" }
-vim.lsp.enable(servers)
+-- 1. Remove "pylsp" from this list
+local servers = { "html", "cssls" }
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- 2. Setup standard servers with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = configs.on_attach,
+    on_init = configs.on_init,
+    capabilities = configs.capabilities,
+  }
+end
+
+-- 3. Setup pylsp SEPARATELY with the ignore rule
+lspconfig.pylsp.setup {
+  on_attach = configs.on_attach,
+  on_init = configs.on_init,
+  capabilities = configs.capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { "E501" },
+          maxLineLength = 120,
+        },
+      },
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -72,6 +72,28 @@ return {
       table.insert(opts.sources, 1, { name = "supermaven" })
     end,
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 

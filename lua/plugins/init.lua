e77cc206437fd45991f798ce93a1c6af6df2a106
NaufalKusumo <nopalji375@gmail.com>
2026-01-08 21:32:18 +0700

    f

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,19 +59,19 @@ return {
     end,
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "supermaven-inc/supermaven-nvim",
-        opts = {},
-      },
-    },
-    opts = function(_, opts)
-      opts.sources[1].trigger_chars = { "-" }
-      table.insert(opts.sources, 1, { name = "supermaven" })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     {
+  --       "supermaven-inc/supermaven-nvim",
+  --       opts = {},
+  --     },
+  --   },
+  --   opts = function(_, opts)
+  --     opts.sources[1].trigger_chars = { "-" }
+  --     table.insert(opts.sources, 1, { name = "supermaven" })
+  --   end,
+  -- },
 
   {
     "kdheepak/lazygit.nvim",
